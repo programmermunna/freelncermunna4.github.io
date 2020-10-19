7dc0c8a5ac5b749288a9594372e21989c3ddd74e
freelancermunna4 <freelancermunna4@gmail.com>
2020-10-20 00:51:59 +0600

    first website
    This is my first webpage

---
+++ ali/js/main.js
@@ -0,0 +1,15 @@
+//slider_area
+
+    $(document).ready(function(){
+      $('.slider_area').bxSlider();
+        
+        
+});
+
+
+    $(document).ready(function(){
+        $('#main_menu').meanmenu({
+            meanMenuContainer:'#mobile_menu',
+            meanScreenWidth: 991,
+        });
+});
